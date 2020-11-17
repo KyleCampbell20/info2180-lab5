ef9b7cc9eff502a1b1e83ae771a90b52dff54c29
KyleCampbell20 <elykcampbell1@gmail.com>
2020-11-17 14:29:36 -0500

    Edited world.php

--- world.php
+++ world.php
@@ -5,16 +5,50 @@ $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
 
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;country=$country;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//$query = filter_input(INPUT_GET, "query", FILTER_SANITIZE_STRING);
 
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-    <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
 
-</ul>
+
+echo "<table border='1'>
+
+
+<tr>
+
+<th>Country Name</th>
+
+<th>Continent</th>
+
+<th>Independence Year</th>
+
+<th>Head of State</th>
+
+</tr>";
+
+ foreach($results as $row): ?>
+<tr>
+
+  <td><?= $row['name']; ?> </td>
+  <td><?= $row['continent']; ?> </td>
+  <td><?= $row['independence_year']; ?> </td>
+  <td><?= $row['head_of_state']; ?> </td>
+
+</tr>
+ <?php endforeach; ?>
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
